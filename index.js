9ca2f49b6ef0c50f5a8eff0dee948626afc54dc0
Daniele Musni <daniele.musni@lc.cuny.edu>
2024-07-12 18:42:59 -0400

    Finished prework with added navbar customization

--- index.js
+++ index.js
@@ -187,30 +187,3 @@ const second = document.createElement('p');
 second.innerHTML = runnerUp.name;
 secondGameContainer.appendChild(second);
 
-
-// own customizations
-let animation = {
-    revealDistance: 150,
-    initialOpacity: 0,
-    transitionDelay: 0,
-    transitionDuration: '2s',
-    transitionProperty: 'all',
-    transitionTimingFunction: 'ease'
-  }
-  
-let revealableContainers = document.querySelectorAll('.revealable');
-  
-const reveal = () => {
-    for(i = 0; i < revealableContainers.length; i++) {
-      let windowHeight = window.innerHeight;
-      let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
-      if (topOfRevealableContainer < windowHeight - animation.revealDistance) {
-        /* add the active class to the revealableContainer's classlist */
-        revealableContainers[i].classList.add('active');
-      } else {
-        /* remove the active class to the revealableContainer's classlist */
-        revealableContainers[i].classList.remove('active');
-      }
-    }
-}
-window.addEventListener('scroll', reveal);
